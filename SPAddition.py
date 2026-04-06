b0ea21d41db650bd30ea2a9c1f38ae940d64b14a
Suhas S. Padaval <suhaspadaval2004@gmail.com>
2026-04-06 11:49:01 +0530

    Add files via upload

---
+++ SPAddition.py
@@ -0,0 +1,15 @@
+1# Start Programe is Basic
+
+# Take Input
+Num1 = int(input('Enter Number 1:'))
+Num2 = int(input('Enter Number 2:'))
+
+#Perform Addition
+sum = Num1 +Num2
+
+#Display Result
+print('Addition is',sum )      #Method is 1
+print('Addition is '+ str(sum))  #Method is 2
+print(f'Addition is{sum}')       #Method is 3
+print(f'Addition of {Num1} and {Num2} is the {sum}') #Method is 4
+
